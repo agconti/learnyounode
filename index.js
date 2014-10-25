63936a2afcf6e6e5ca8ebf6a3e07231b79831b21
agconti <agc11d@gmail.com>
2014-10-24 21:02:09 -0400

    feat(lesson/2): completed

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("HELLO WORLD");
+// starts at two because we expect this program to always be
+// run from the commandline in the format:
+//
+// `node programName.js [arguments]`
+//
+// therefore the first two items in process.argv can safely be skipped.
+
+var i = 2, 
+    argLength = process.argv.length,
+    sum = 0;
+
+for(; i<argLength; i++){
+    sum += +process.argv[i];
+}
+console.log(sum);
