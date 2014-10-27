2a561a1e8cf364b62d0b4d971a124747c8c174c8
agconti <agc11d@gmail.com>
2014-10-27 09:58:19 -0400

    feat(lesson/9): complete

--- index.js
+++ index.js
@@ -1,32 +1,52 @@
 ;(function(){
-// require the file system module
+
+// require the http module
 var http = require('http')
-    , url = process.argv[2]
-    , responseBody = ""
+  , url1 = process.argv[2]
+  , url2 = process.argv[3]
+  , url3 = process.argv[4]
 
+var urlArr = [ url1
+             , url2
+             , url3 ]
 
+function logResults(err, responseBody){
 
-function logResults(){
-    console.log(responseBody.length)
+    if (err) { console.error(err) }
+
     console.log(responseBody)
 }
 
 function callback(response){
+    
+    // set response body as a closure
+    var responseBody = ''
     
     // set response to string instead of buffer
     response.setEncoding('utf8')
 
     // data:event
     response.on('data', function(chunk){
-       responseBody += chunk
+       responseBody += chunk    
     })
 
     // log errors to the console
     response.on('error', console.error)
 
-    // log results once the server is finsihed
-    response.on('end', logResults)
+    // call the logResults callback once 
+    // the server is finsihed responding
+    response.on('end', function(){
+        logResults(null, responseBody)
+        
+        // now that the server is finsihed
+        // invoke the next call 
+        var url = urlArr.shift()
+        if (url){
+            http.get(url, callback)
+        }
+    })
 }
 
-http.get(url, callback)
+http.get(urlArr.shift(), callback)
+
 })()
