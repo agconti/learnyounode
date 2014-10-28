35ea3a4f95ed5e4b9e02e7e0aefdfe3f8b975fc5
agconti <agc11d@gmail.com>
2014-10-28 17:05:33 -0400

    feat(lesson/12): complete

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 ;(function(){
   var http = require('http')
-    , fs = require('fs')
+    , map = require('through2-map')
 
   http.createServer(function (req, res) {
 
-    // respond with a status + content type
-    res.writeHead(200, { 'content-type': 'text/plain' })
+     if (req.method != 'POST') {
+        return res.end('Only post requests are accpted\n')
+      }
     
-    // read the file and pipe the response to the client
-    fs.createReadStream(process.argv[3]).pipe(res)
-  }).listen(process.argv[2])
+    req.pipe(map(function (chunk) {
+      return chunk.toString().toUpperCase()
+    })).pipe(res)
   
+  }).listen(Number(process.argv[2]))
+
 })()
