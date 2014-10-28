9612c1232165ed003235f2427344edaeccf1ee0a
agconti <agc11d@gmail.com>
2014-10-27 20:49:49 -0400

    feat(lesson/10): complete

--- index.js
+++ index.js
@@ -1,52 +1,24 @@
 ;(function(){
 
-// require the http module
-var http = require('http')
-  , url1 = process.argv[2]
-  , url2 = process.argv[3]
-  , url3 = process.argv[4]
+var net = require('net')
+  , port = process.argv[2]
+  , server 
 
-var urlArr = [ url1
-             , url2
-             , url3 ]
-
-function logResults(err, responseBody){
-
-    if (err) { console.error(err) }
-
-    console.log(responseBody)
+// user strftime once you get internet
+function getDate(){
+    var date = new Date()
+    return [ date.getFullYear() + '-'
+           , ( date.getMonth() + 1 ) + '-'  
+           , date.getDate() + ' ' 
+           , date.getHours() + ":"
+           , date.getMinutes()
+           ].join("")
 }
 
-function callback(response){
-    
-    // set response body as a closure
-    var responseBody = ''
-    
-    // set response to string instead of buffer
-    response.setEncoding('utf8')
-
-    // data:event
-    response.on('data', function(chunk){
-       responseBody += chunk    
-    })
-
-    // log errors to the console
-    response.on('error', console.error)
-
-    // call the logResults callback once 
-    // the server is finsihed responding
-    response.on('end', function(){
-        logResults(null, responseBody)
-        
-        // now that the server is finsihed
-        // invoke the next call 
-        var url = urlArr.shift()
-        if (url){
-            http.get(url, callback)
-        }
-    })
-}
-
-http.get(urlArr.shift(), callback)
+var server = net.createServer(function (socket) {
+  // socket handling logic
+  socket.end(getDate())
+})
+server.listen(port)
 
 })()
