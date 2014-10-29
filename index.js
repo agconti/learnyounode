89d27889f4b0d0ee3bbcfeb55bdc7aa22c236c0f
agconti <agc11d@gmail.com>
2014-10-29 11:28:12 -0400

    feat(lesson/13): completed

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
 ;(function(){
   var http = require('http')
-    , map = require('through2-map')
+    , url = require('url')
+    , strftime = require('strftime')
 
   http.createServer(function (req, res) {
 
-     if (req.method != 'POST') {
-        return res.end('Only post requests are accpted\n')
-      }
-    
-    req.pipe(map(function (chunk) {
-      return chunk.toString().toUpperCase()
-    })).pipe(res)
-  
+    var parms = url.parse(req.url, true)
+
+    switch (parms.pathname) {
+
+      case '/api/parsetime':
+        res.writeHead(200, { 'Content-Type': 'application/json' })
+
+        var date = new Date(parms.query.iso)
+          , jsonResponse = {
+                "hour": date.getHours()
+              , "minute": date.getMinutes()
+              , "second": date.getSeconds()
+            }
+        
+        res.end(JSON.stringify(jsonResponse))
+        break
+      
+      case '/api/unixtime':
+        res.writeHead(200, { 'Content-Type': 'application/json' })
+         var date = new Date(parms.query.iso)
+          , jsonResponse = {"unixtime": date.getTime()}
+        res.end(JSON.stringify(jsonResponse))
+        break
+
+      default:
+        res.writeHead(404, { 'Content-Type': 'application/text' })
+        res.end("Not Found")
+        
+    }
+
   }).listen(Number(process.argv[2]))
 
 })()
